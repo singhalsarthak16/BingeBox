f914ed2b6e2b200910b8b8d8ec8df706c8d0cdfe
singhalsarthak16 <singhalsarthak1612@gmail.com>
2025-03-31 16:49:08 +0530

    icon fixed

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,7 +1,7 @@
 
 import { Link } from "react-router-dom";
 
-const NotFoundPage = () => {
+const NotFoundPage = () => { 
 	return (
 		<div
 			className='min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white'
